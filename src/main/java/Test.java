e569cf98deffc4d743ef0414f8ffb17b244abd33
Administrator <Administrator@DESKTOP-DNR185M>
2019-06-19 09:49:57 +0800

    dddd

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("1111");
+		String str = "<br/>|<Br/>sccsds<br/>|<BR/><br/>|<BR/><br/>|<BR/><br/>|<BR/>";
+		str =  str.replaceAll("<br/>|<BR/>", "1212");
+		System.out.println(str);
 	}
 }
