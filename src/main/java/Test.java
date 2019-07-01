de8fc0d3c786aaa6dcda99bae3bed645d8761f0a
Administrator <Administrator@DESKTOP-DNR185M>
2019-07-01 08:59:18 +0800

    是发v

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,10 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Test {
 	public static void main(String[] args) {
-		String str = "<br/>|<Br/>sccsds<br/>|<BR/><br/>|<BR/><br/>|<BR/><br/>|<BR/>";
-		str =  str.replaceAll("<br/>|<BR/>", "1212");
-		System.out.println(str);
+		ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(2);
+		try {
+			queue.put(10);
+			queue.put(9);
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+			queue.put(8);
+			System.out.println(queue.take());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+class testCla{
+	private String name="11111";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
